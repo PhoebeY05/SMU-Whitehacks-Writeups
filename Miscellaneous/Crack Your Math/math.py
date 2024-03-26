5e8386b6824cab9a256715ba80c81c18a7b97f05
Phoebe Yap <115935747+PhoebeY05@users.noreply.github.com>
2024-03-26 10:26:31 +0800

    Update math.py

--- Miscellaneous/Crack Your Math/math.py
+++ Miscellaneous/Crack Your Math/math.py
@@ -4,13 +4,14 @@ HOST = "ctf2.whitehats.site"
 PORT = 4001
 
 tn = telnetlib.Telnet(HOST, PORT)
-
+def remove(string):
+    return string.replace(" ", "")
+    
 while True:
     print(tn.read_until(b"/ 123\n"))
     question = tn.read_until(b"\n").decode()
     print(question)
-    def remove(string):
-        return string.replace(" ", "")
+    
     question = remove(question)
     result = numexpr.evaluate(question).item()
     print(result)
